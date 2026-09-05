5aa3a091e40573b3ee1503db36be1a2e6f8c0224
Lakshya V. <92231817+LakshyaV@users.noreply.github.com>
2026-09-05 02:34:21 -0400

    Sync client/src/components/examples/BottomNav.tsx

---
+++ client/src/components/examples/BottomNav.tsx
@@ -0,0 +1,9 @@
+import BottomNav from '../BottomNav';
+
+export default function BottomNavExample() {
+  return (
+    <div className="h-screen flex items-end">
+      <BottomNav />
+    </div>
+  );
+}
